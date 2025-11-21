e4d726db1c423e7e09fe333428831688affa4c2a
matteokeole <matteoo34@icloud.com>
2025-11-21 13:43:20 +0100

    fix: Colors in cmd.exe

--- scroll/base.hpp
+++ scroll/base.hpp
@@ -13,6 +13,14 @@
 	#include "Base/Base/Base.hpp"
 #endif
 
+#if ATL_OPERATING_SYSTEM == ATL_OPERATING_SYSTEM_WINDOWS
+	#include <Windows.h>
+
+	#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+		#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+	#endif
+#endif
+
 namespace scroll {
 	using atl::uint8;
 	using atl::uint16;
